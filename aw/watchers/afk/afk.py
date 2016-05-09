a99ce4c696f5d722590a663243e76e5ac62c9a2b
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-05-09 16:24:31 +0200

    Plenty of changes, now uses argparse and started working on event_data for more info about what happened in an AFK/non-AFK period

--- aw/watchers/afk/afk.py
+++ aw/watchers/afk/afk.py
@@ -2,7 +2,7 @@ import logging
 import platform
 from datetime import datetime, timedelta
 from time import sleep
-from threading import Event, Thread
+import threading
 import json
 import socket
 import sys
@@ -12,33 +12,40 @@ import requests
 from pykeyboard import PyKeyboard, PyKeyboardEvent
 from pymouse import PyMouse, PyMouseEvent
 
+from aw.core.models import Event
 from aw.client import ActivityWatchClient
 
-
+# TODO: Move to argparse
 settings = {
     "timeout": 60,
     "check_interval": 1,
-    "server_enabled": True,
-    "desktop_notify": True,
 }
 
 logger = logging.getLogger("aw-watcher-afk")
 logger.setLevel(logging.DEBUG)
 
-if settings["desktop_notify"]:
-    from gi.repository import Notify
-    Notify.init("afkwatcher")
-
-def send_notification(msg):
-    if settings["desktop_notify"]:
-        # Can crash the application if the notification daemon disappears
-        n = Notify.Notification.new("AFK state changed", msg)
-        n.show()
-
 
 def main():
+    import argparse
+
+    parser = argparse.ArgumentParser("A watcher for keyboard and mouse input to detect AFK state")
+    parser.add_argument("--testing", dest="testing", action="store_const", const=True, default=False)
+    parser.add_argument("--desktop-notify", dest="desktop_notify", action="store_const", const=True, default=False)
+
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-    client = ActivityWatchClient("afkwatcher")
+    client = ActivityWatchClient("afkwatcher", testing=args.testing)
+
+    if args.desktop_notify:
+        from gi.repository import Notify
+        Notify.init("afkwatcher")
+
+    def send_notification(msg):
+        if args.desktop_notify:
+            # Can crash the application if the notification daemon disappears
+            n = Notify.Notification.new("AFK state changed", msg)
+            n.show()
 
     now = datetime.now()
     last_activity = now
@@ -55,13 +62,13 @@ def main():
         logger.warning("KeyboardListener is broken in OS X, will not use for detecting AFK state.")
 
     logger.info("afkwatcher started")
-    client.send_event({"label": "afkwatcher-started", "settings": settings})
+    client.send_event(Event(label="afkwatcher-started", settings=settings))
 
     def change_to_afk(dt: datetime):
         # This function should be called when user becomes AFK
         # The argument dt should be the time when the last activity was detected,
         # which should be: change_to_afk(dt=last_activity)
-        client.send_event({"label": "not-afk", "timestamp": dt.isoformat()})
+        client.send_event(Event(label="not-afk", timestamp=dt))
         logger.info("Now AFK")
         send_notification("Now AFK")
         nonlocal is_afk
@@ -71,7 +78,7 @@ def main():
         # This function should be called when user is no longer AFK
         # The argument dt should be the time when the at-keyboard indicating activity was detected,
         # which should be: change_to_not_afk(dt=now)
-        client.send_event({"label": "afk", "timestamp": dt.isoformat()})
+        client.send_event(Event(label="afk", timestamp=dt))
         logger.info("No longer AFK")
         send_notification("No longer AFK")
         nonlocal is_afk
@@ -83,7 +90,7 @@ def main():
         try:
             sleep(settings["check_interval"])
             now = datetime.now()
-            if mouseListener.has_new_activity() or keyboardListener.has_new_activity():
+            if mouseListener.has_new_event() or keyboardListener.has_new_event():
                 # Check if there has been any activity on the mouse or keyboard and if so,
                 # update last_activity to now and set is_afk to False if previously AFK
                 if is_afk:
@@ -107,53 +114,96 @@ def main():
 
         except KeyboardInterrupt:
             logger.info("afkwatcher stopped by keyboard interrupt")
-            client.send_event({"label": "afkwatcher-stopped"})
+            client.send_event(Event(label="afkwatcher-stopped"))
             break
         except Exception as e:
             logger.warning("afkwatcher stopped by unexpected exception")
-            client.send_event({"label": "afkwatcher-stopped", "note": str(e)})
+            client.send_event(Event(label="afkwatcher-stopped", note=str(e)))
             break
 
 
-class KeyboardListener(PyKeyboardEvent):
+class EventFactory:
+    def next_event(self):
+        """Returns an event and prepares the internal state so that it can start to build a new event"""
+        raise NotImplementedError
+
+    def has_new_event(self):
+        answer = self.new_activity.is_set()
+        return answer
+
+
+class KeyboardListener(PyKeyboardEvent, EventFactory):
     def __init__(self):
         PyKeyboardEvent.__init__(self)
-        self.logger = logging.getLogger("afkwatcher.keyboard")
+        self.logger = logging.getLogger("aw.watchers.afk.keyboard")
         self.logger.setLevel(logging.INFO)
-        self.activity_event = Event()
+        self.new_activity = threading.Event()
+        self._reset_data()
+
+    def _reset_data(self):
+        self.event_data = {
+            "presses": 0
+        }
 
     def tap(self, keycode, character, press):
         #logging.debug("Clicked keycode: {}".format(keycode))
         self.logger.debug("Input received: {}, {}, {}".format(keycode, character, press))
-        self.activity_event.set()
+        self.event_data["presses"] += 1
+        self.new_activity.set()
 
     def escape(self, event):
         # Always returns False so that listening is never stopped
         return False
 
-    def has_new_activity(self):
-        answer = self.activity_event.is_set()
-        self.activity_event.clear()
-        return answer
+    def next_event(self):
+        """Returns an event and prepares the internal state so that it can start to build a new event"""
+        self.new_activity.clear()
+        data = self.event_data
+        self._reset_data()
+        return data
 
 
-class MouseListener(PyMouseEvent):
+class MouseListener(PyMouseEvent, EventFactory):
     def __init__(self):
         PyMouseEvent.__init__(self)
-        self.logger = logging.getLogger("afkwatcher.mouse")
+        self.logger = logging.getLogger("aw.watchers.afk.mouse")
         self.logger.setLevel(logging.INFO)
-        self.activity_event = Event()
+        self.new_activity = threading.Event()
+        self.pos = None
+        self._reset_data()
+
+    def _reset_data(self):
+        self.event_data = {
+            "clicks": 0,
+            "deltaX": 0,
+            "deltaY": 0
+        }
 
     def click(self, x, y, button, press):
+        # TODO: Differentiate between leftclick and rightclick?
         self.logger.debug("Clicked mousebutton: {}".format(button))
-        self.activity_event.set()
+        self.event_data["clicks"] += 1
+        self.new_activity.set()
 
     def move(self, x, y):
+        newpos = (x, y)
+        if not self.pos:
+            self.pos = newpos
+        else:
+            delta = tuple(abs() for i in range(2))
+            self.event_data["deltaX"] += newpos[0] - self.pos[0]
+            self.event_data["deltaY"] += newpos[1] - self.pos[1]
         self.logger.debug("Moved mouse to: {},{}".format(x, y))
-        self.activity_event.set()
+        self.new_activity.set()
 
     def has_new_activity(self):
-        answer = self.activity_event.is_set()
-        self.activity_event.clear()
+        answer = self.new_activity.is_set()
+        self.new_activity.clear()
         return answer
+
+    def next_event(self):
+        self.new_activity.clear()
+        data = self.event_data
+        self._reset_data()
+        return data
 
