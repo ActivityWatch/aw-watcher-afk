ac104d1ca7ea4eba0cadbc5aaf95e593df170d74
johan-bjareholt <johanthepro@gmail.com>
2016-08-22 20:05:48 +0200

    Moved listeners to their own file

--- aw_watcher_afk/afk.py
+++ aw_watcher_afk/afk.py
@@ -1,15 +1,13 @@
 import logging
 import platform
 from datetime import datetime, timedelta
-from time import sleep
-import threading
-
-from pykeyboard import PyKeyboardEvent
-from pymouse import PyMouseEvent
 import pytz
+from time import sleep
 
 from aw_core.models import Event
 from aw_client import ActivityWatchClient
+
+from .listeners import KeyboardListener, MouseListener
 
 # TODO: Move to argparse
 settings = {
@@ -153,92 +151,3 @@ def main():
             logger.warning("afkwatcher stopped by unexpected exception:\n{}".format(str(e)))
             break
 
-
-class EventFactory:
-    def next_event(self):
-        """Returns an event and prepares the internal state so that it can start to build a new event"""
-        raise NotImplementedError
-
-    def has_new_event(self):
-        raise NotImplementedError
-
-
-class KeyboardListener(PyKeyboardEvent, EventFactory):
-    def __init__(self):
-        PyKeyboardEvent.__init__(self)
-        self.logger = logging.getLogger("aw.watchers.afk.keyboard")
-        self.logger.setLevel(logging.INFO)
-        self.new_event = threading.Event()
-        self._reset_data()
-
-    def _reset_data(self):
-        self.event_data = {
-            "presses": 0
-        }
-
-    def tap(self, keycode, character, press):
-        # logging.debug("Clicked keycode: {}".format(keycode))
-        self.logger.debug("Input received: {}, {}, {}".format(keycode, character, press))
-        self.event_data["presses"] += 1
-        self.new_event.set()
-
-    def escape(self, event):
-        # Always returns False so that listening is never stopped
-        return False
-
-    def next_event(self):
-        """Returns an event and prepares the internal state so that it can start to build a new event"""
-        self.new_event.clear()
-        data = self.event_data
-        self._reset_data()
-        return data
-
-    def has_new_event(self):
-        return self.new_event.is_set()
-
-
-class MouseListener(PyMouseEvent, EventFactory):
-    def __init__(self):
-        PyMouseEvent.__init__(self)
-        self.logger = logging.getLogger("aw.watchers.afk.mouse")
-        self.new_event = threading.Event()
-        self.pos = None
-        self._reset_data()
-
-    def _reset_data(self):
-        self.event_data = {
-            "clicks": 0,
-            "deltaX": 0,
-            "deltaY": 0
-        }
-
-    def click(self, x, y, button, press):
-        # TODO: Differentiate between leftclick and rightclick?
-        if press:
-            self.logger.debug("Clicked mousebutton: {}".format(button))
-            self.event_data["clicks"] += 1
-        self.new_event.set()
-
-    def move(self, x, y):
-        newpos = (x, y)
-        #self.logger.debug("Moved mouse to: {},{}".format(x, y))
-        if not self.pos:
-            self.pos = newpos
-
-        delta = tuple(abs(self.pos[i] - newpos[i]) for i in range(2))
-        self.event_data["deltaX"] += delta[0]
-        self.event_data["deltaY"] += delta[1]
-
-        self.pos = newpos
-        self.new_event.set()
-
-    def has_new_event(self):
-        answer = self.new_event.is_set()
-        self.new_event.clear()
-        return answer
-
-    def next_event(self):
-        self.new_event.clear()
-        data = self.event_data
-        self._reset_data()
-        return data
